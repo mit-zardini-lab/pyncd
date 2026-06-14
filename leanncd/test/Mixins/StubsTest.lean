f8b6d6dd9c91b6dea2e72f7ef0f1b542c5df34bc
William Macready <william.macready@gmail.com>
2026-06-14 06:52:21 -0700

    feat(leanncd): add RouteStructure and SymmetryGraded stub mixins

---
+++ leanncd/test/Mixins/StubsTest.lean
@@ -0,0 +1,13 @@
+import Mathlib
+import LeanNCD.Mixins.Route
+import LeanNCD.Mixins.Symmetry
+
+namespace LeanNCD
+open CategoryTheory
+
+example {D C : Type} [ColoredPROP D] [ColoredPROP C] [RouteStructure D C] :
+    DGradedColoredPROP D C := inferInstance
+example {D C : Type} [ColoredPROP D] [ColoredPROP C] (T : Monad D) [SymmetryGraded D C T] :
+    DGradedColoredPROP D C := inferInstance
+
+end LeanNCD
